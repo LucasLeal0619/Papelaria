13a210de1fdf9f9a8b366e76d0f7c2305b4d6da9
Stephanny Freitas <stephanny.freitas1234@gmail.com>
2026-05-07 08:33:31 -0300

    altscript

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
 function enviar(){
     window.confirm("Deseja enviar seu formulário agora?")
-}
-
-function botaoFormulario(){
+    
     window.alert ("Seu formulário foi recebido com sucesso!")
 }
+
